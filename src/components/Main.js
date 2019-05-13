bae26edac73dd37feee4d40ed3a37dd1367dc820
Konstantin podlesny <konstantin-921@yandex.ru>
2019-05-13 20:22:44 +0300

    Rework components

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,57 +9,12 @@ socket.on('news', function (data) {
 });
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataMLB: {},
-      dataNBA: {},
-      error: null,
-      loading: true,
-    }
-  }
-
-  componentDidMount() {
-    socket.on('my other event', function(data){
-      console.log(data);
-    });
-    const p1 = axios.get('http://localhost:4444/api/games', { params: { league: 'MLB' }})
-    const p2 = axios.get('http://localhost:4444/api/games', { params: { league: 'NBA' }})
-
-    Promise.all([p1, p2])
-    .then( ([res1, res2]) => {
-      this.setState({
-        loading: false,
-        dataNBA: res2.data,
-        dataMLB: res1.data
-      })
-    })
-    .catch(err => {
-      console.error(err);
-      this.setState({
-        error: err,
-        loading: false
-      })
-    })
-  }
 
   render() {
-    const data = this.state;
-    const { loading } = this.state;
     return (
-      // <div>
-      //   <MainTable league="NBA" />
-      //   <MainTable league="MLB" />
-      // </div>
-      <div>
-        {!loading ? 
-        <React.Fragment>
-          <div style={{minWidth: '410px', maxWidth: '1000px', margin: 'auto'}}>
-            <MainTable league='MLB' data={data.dataMLB} />
-            <MainTable league='NBA' data={data.dataNBA} />
-          </div>
-        </ React.Fragment> : null
-        }
+      <div style={{minWidth: '410px', maxWidth: '1000px', margin: 'auto'}}>
+        <MainTable league='MLB' />
+        <MainTable league="NBA" />
       </div>
     );
   }
--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -27,93 +27,97 @@ class MainTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataMLB: {},
-      dataNBA: {},
+      game: {},
       error: null,
       loading: true,
     }
   }
 
-  // componentDidMount() {
-  //   axios.get('http://localhost:4444/api/games', { params: { league: this.props.league }})
-  //     .then(response => {
-
-  //     })
-  // }
+  componentDidMount() {
+    const { league } = this.props
+    axios.get('http://localhost:4444/api/games', { params: { league }})
+    .then(response => {
+      this.setState({
+        loading: false,
+        game: response.data,
+      })
+    })
+    .catch(err => {
+      console.error(err);
+      this.setState({
+        error: err,
+        loading: false
+      })
+    })
+  }
 
   getSideTable = (league) => {
     const LEAGUE = {
-      MLB: <MlbSideTable data={this.props.data} />,
-      NBA: <NbaSideTable data={this.props.data} />
+      MLB: <MlbSideTable data={this.state.game} />,
+      NBA: <NbaSideTable data={this.state.game} />
     }
     return LEAGUE[league]
   }
   
   getFooter = (league) => {
     const FOOTER = {
-      MLB: <MlbFooter data={this.props.data} />,
-      NBA: <NbaFooter data={this.props.data} />
+      MLB: <MlbFooter data={this.state.game} />,
+      NBA: <NbaFooter data={this.state.game} />
     }
     return FOOTER[league]
   }
   
   render() {
-    const { classes } = this.props
-    const { league } = this.props.data
-    const periodsAway = this.props.data.away_period_scores
-    const periodsHome = this.props.data.home_period_scores
-    const awayTeam = this.props.data.away_team.abbreviation
-    const homeTeam = this.props.data.home_team.abbreviation
+    const { loading, game } = this.state
+    if(loading) { return null }
+    const { classes, league } = this.props
+    const periodsAway = game.away_period_scores
+    const periodsHome = game.home_period_scores
+    const awayTeam = game.away_team.abbreviation
+    const homeTeam = game.home_team.abbreviation
     const renderLeague = this.getSideTable(league)
     const footer = this.getFooter(league)
     return (
-      <React.Fragment>
-        <Grid container spacing={0} direction="row" alignItems="center" justify="center">
-          <Grid item xs={9}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell className={classes.tableCell} style={{ backgroundColor: '#f0f0f0' }}></TableCell>
-                    {periodsAway.map((item, index) => {
-                      return <TableCell className={classes.tableCell} key={index}>{index + 1}</TableCell>
-                    })}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className={classes.tableCell} style={{ backgroundColor: '#f0f0f0', fontWeight: 'bold' }}>{awayTeam}</TableCell>
-                    {periodsAway.map((item, index) => {
-                      return <TableCell className={classes.tableCell} key={index}>{item}</TableCell>
-                    })}
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className={classes.tableCell} style={{ backgroundColor: '#f0f0f0', fontWeight: 'bold' }}>{homeTeam}</TableCell>
-                    {periodsHome.map((item, index) => {
-                      return <TableCell className={classes.tableCell} key={index}>{item}</TableCell>
-                    })}
-                  </TableRow>
-                </TableBody>
-              </Table>
+      <div>
+        <React.Fragment>
+          <Grid container spacing={0} direction="row" alignItems="center" justify="center">
+            <Grid item xs={9}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell className={classes.tableCell} style={{ backgroundColor: '#f0f0f0' }}></TableCell>
+                      {periodsAway.map((item, index) => {
+                        return <TableCell className={classes.tableCell} key={index}>{index + 1}</TableCell>
+                      })}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className={classes.tableCell} style={{ backgroundColor: '#f0f0f0', fontWeight: 'bold' }}>{awayTeam}</TableCell>
+                      {periodsAway.map((item, index) => {
+                        return <TableCell className={classes.tableCell} key={index}>{item}</TableCell>
+                      })}
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className={classes.tableCell} style={{ backgroundColor: '#f0f0f0', fontWeight: 'bold' }}>{homeTeam}</TableCell>
+                      {periodsHome.map((item, index) => {
+                        return <TableCell className={classes.tableCell} key={index}>{item}</TableCell>
+                      })}
+                    </TableRow>
+                  </TableBody>
+                </Table>
+            </Grid>
+            {renderLeague}
           </Grid>
-          {renderLeague}
-        </Grid>
-        {footer}
-      </React.Fragment>
+          {footer}          
+        </React.Fragment>
+      </div>
     );
   }
 }
 
 MainTable.propTypes = {
-  data: PropTypes.shape({
-    away_period_scores: PropTypes.arrayOf(PropTypes.number).isRequired,
-    home_period_scores: PropTypes.arrayOf(PropTypes.number).isRequired,
-    away_team: PropTypes.shape({
-      abbreviation: PropTypes.string.isRequired,
-    }).isRequired,
-    home_team: PropTypes.shape({
-      abbreviation: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
+  league: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(MainTable)
