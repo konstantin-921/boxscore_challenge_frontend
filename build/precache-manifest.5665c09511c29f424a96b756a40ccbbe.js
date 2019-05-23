c536574c27e351379f707d88dddfb1ab36247a41
Konstantin podlesny <konstantin-921@yandex.ru>
2019-05-23 11:32:49 +0300

    Fiz .gitinore for deployment

---
+++ build/precache-manifest.5665c09511c29f424a96b756a40ccbbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb92c7aed7617f96509252813199aea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae8c2ffd9b23782b907",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "edce8ea35a7ad12dd330",
+    "url": "/static/js/2.4ca1595e.chunk.js"
+  },
+  {
+    "revision": "7ae8c2ffd9b23782b907",
+    "url": "/static/js/main.a69c1d91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
