ab98fd8c58ca71c2ebed1cdacb417077bf743956
Konstantin podlesny <konstantin-921@yandex.ru>
2019-05-24 13:56:57 +0300

    Fix env port api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? 'http://3.92.175.31/' : process.env.PORT_API_URL;
+const apiUrl = process.env.NODE_ENV === 'production' ? 'http://3.92.175.31/' : process.env.REACT_APP_PORT_API_URL;
 
 const config = {
   BASE_URL: apiUrl,
