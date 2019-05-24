c1d6687ca7bd5512a8f3b11cf9cf56f7ab5dac9a
Konstantin podlesny <konstantin-921@yandex.ru>
2019-05-24 14:49:18 +0300

    Add elasticIP to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? 'http://3.92.175.31/' : process.env.REACT_APP_PORT_API_URL;
+const apiUrl = process.env.NODE_ENV === 'production' ? 'http://18.205.171.18/' : process.env.REACT_APP_PORT_API_URL;
 
 const config = {
   BASE_URL: apiUrl,
