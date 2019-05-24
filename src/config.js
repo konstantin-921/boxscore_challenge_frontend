d6d78705079b8d7785d53992bcfb1ab09f38e540
Konstantin podlesny <konstantin-921@yandex.ru>
2019-05-24 13:36:26 +0300

    Add env var port api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+const apiUrl = process.env.NODE_ENV === 'production' ? 'http://3.92.175.31' : process.env.PORT_API_URL;
+
 const config = {
-  BASE_URL: 'http://localhost:3000/',
+  BASE_URL: apiUrl,
 }
 
 export default config
